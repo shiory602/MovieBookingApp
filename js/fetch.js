791b7568ff2a06a219ab7a088d8101cb2866ed62
shiory602 <suzushioufo@gmail.com>
2021-04-30 16:14:52 -0700

    fetch and convert genre data

--- js/fetch.js
+++ js/fetch.js
@@ -2,35 +2,76 @@
 // https://www.themoviedb.org/settings/api
 
 const API_KEY = '0872d5dd1f8ee0ee90233d854bce0ad4';
-let title = 'あんぱんまん'
-let url = `https://api.themoviedb.org/3/movie/157336?api_key=${API_KEY}&append_to_response=videos,images`;
-let url_now_playing = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&append_to_response=videos,images`;
+let url_genres = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`;
 let url_popular = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`;
+// let MOVIE_ID = getPopularMovieData().forEach(el => el);
+// console.log(MOVIE_ID);
+// let url_detail = `https://api.themoviedb.org/3/movie/${MOVIE_ID}?api_key=${API_KEY}&append_to_response=videos,images`;
 
-console.log(url_now_playing);
+let url_general = `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`;
+let url_now_playing = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&append_to_response=videos,images`;
+
+// console.log(url_detail);
 
 
 
-export function getNowPlayingMovieData() {
-    return fetch(url_now_playing)
-        .then(res => {
-            return res.json();
-        });
+
+/*-------------------------------
+Checking Error function
+-------------------------------*/
+let checkError = res => {
+    if (res.status !== 200) {
+        alert('We have an error ${res.status}');
+    }
 }
 
+/*-------------------------------
+Movie Categories
+-------------------------------*/
+export function getMovieCategories() {
+    return fetch(url_genres)
+    .then(res => {
+        checkError(res);
+        return res.json();
+    });
+}
+
+
+/*--------------------------------------------------------------*/
+
+/*-------------------------------
+Popular
+-------------------------------*/
 export function getPopularMovieData() {
-
     return fetch(url_popular)
-
         .then(res => {
+            checkError(res);
             return res.json();
-        }) // .thenが後ろにあったらreturnつける
-
-        .then(data => { // data = res.json()
-
-            console.log(data);
-
         })
-
+        .then(data => {
+            let popularId = data.results.map(i => i.id);
+            // console.log(popularId);
+            return popularId;
+        })
         .catch(error => console.error(error));
 }
+
+
+/*-------------------------------
+Movie Title
+-------------------------------*/
+// export function getMovieDetail() {
+//     return fetch(url_detail)
+//     .then(res => {
+//         checkError(res);
+//         return res.json();
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => console.error(error));
+// }
+
+/*-------------------------------
+Movie Picture
+-------------------------------*/
--- js/main.js
+++ js/main.js
@@ -1,14 +1,34 @@
-import { getNowPlayingMovieData, getPopularMovieData } from "./fetch.js";
-import { startCarousel } from "./carousel.js";
+import {
+    getMovieCategories,
+    getPopularMovieData
+} from "./fetch.js";
+import {
+    startCarousel
+} from "./carousel.js";
+
+const genreButtons = document.querySelector('.genres');
 
 startCarousel();
 
-getNowPlayingMovieData()
+getMovieCategories()
     .then(data => {
-        console.log(data);
+        const categoryName = data.genres.map(i => i.name); // category Name array
+        // console.log(categoryName.length);
+        const categoryId = data.genres.map(i => i.id); // category ID array
+        // console.log(categoryId)
+
+        categoryName.forEach(el => {
+            genreButtons.innerHTML += `
+                <a href="#">
+                    <div class="genre-item">
+                        <h3>${el}</h3>
+                    </div>
+                </a>
+            `;
+        });
     })
     .catch(error => console.error(error));
 
 
-    
-getPopularMovieData();
+
+getPopularMovieData()
