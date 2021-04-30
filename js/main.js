4e8ac9f9bab48bbfce33b76b80027f49d2e64544
shiory602 <suzushioufo@gmail.com>
2021-04-30 16:46:23 -0700

    Add genre section and fix layout

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ import {
 } from "./carousel.js";
 
 const genreButtons = document.querySelector('.genres');
+const genreJS = document.querySelector('.genreJS');
 
 startCarousel();
 
@@ -17,15 +18,38 @@ getMovieCategories()
         const categoryId = data.genres.map(i => i.id); // category ID array
         // console.log(categoryId)
 
+        /*----------------------
+        Category Buttons
+        -----------------------*/
         categoryName.forEach(el => {
             genreButtons.innerHTML += `
-                <a href="#">
+                <a href="#${el}">
                     <div class="genre-item">
                         <h3>${el}</h3>
                     </div>
                 </a>
             `;
         });
+
+        /*----------------------
+        Genre Section
+        ----------------------*/
+        categoryName.forEach(el => {
+            genreJS.innerHTML += `
+                <h2 id="${el}">${el}</h2>
+                <div class="films">
+                    <div class="film-item">
+                        <a href="./movieDetail.html" target="_blank" rel="noopener noreferrer">
+                            <img src="./img/ladyandthetramp.png" alt="">
+                            <h4>title</h4>
+                            <p>category</p>
+                        </a>
+                    </div>
+                </div>
+            `;
+        })
+        // genreJS.appendChild();
+
     })
     .catch(error => console.error(error));
 
