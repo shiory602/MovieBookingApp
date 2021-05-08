6e59441066bc7e3cf96bc8d58e38bb04824c6b7e
shiory602 <suzushioufo@gmail.com>
2021-05-07 22:13:39 -0700

    description data

--- js/movieDetail.js
+++ js/movieDetail.js
@@ -2,10 +2,11 @@ import {
     getMovieCategories
 } from "./fetch.js";
 
-const movieTitle = document.querySelector('.movie-detail-description h4');
-const movieCategory = document.querySelector('.movie-detail-description span');
-const movieRate = document.querySelector('.movie-detail-description .rate');
-const movieRate = document.querySelector('.movie-detail-description .rate');
+const movieTitle = document.querySelector('#movie-title');
+const movieCategory = document.querySelector('#category-list');
+const movieOverview = document.querySelector('#overview');
+const releaseDate = document.querySelector('#release-date');
+const voteAverage = document.querySelector('#vote-average');
 
 const movieBg = document.querySelector('.movie-detail-bg');
 
@@ -19,7 +20,20 @@ console.log(url_detail);
 fetch(url_detail).then((response) => response.json())
     .then((json) => {
         movieTitle.textContent = json.title;
-        console.log(json);
+
+        for (const genre of json.genres) {
+            // <span></span>
+            const genreEl = document.createElement("span");
+            // <span>Action</span>
+            genreEl.textContent = genre.name;
+            genreEl.setAttribute("class", "category");
+
+            movieCategory.insertAdjacentElement("beforeend", genreEl);
+        }
+
+        releaseDate.textContent = `Release: ${json.release_date}`;
+        voteAverage.textContent = `★${json.vote_average}`;
+        movieOverview.textContent = json.overview;
 
         movieBg.innerHTML = `<img src="https://image.tmdb.org/t/p/w500${json.backdrop_path}" alt="${json.title} trailer">`
     });
